26156fe9618439a3d5ee9c178c519071a623b677
Avtandil-1 <avtandil.merebashvili.2@gmail.com>
2022-09-06 22:46:51 +0400

    feat(components): refactor to reactiveFormsModule

--- src/app/common/components/dropdown/dropdown.module.ts
+++ src/app/common/components/dropdown/dropdown.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DropdownComponent } from './dropdown.component';
 import { MatSelectModule } from '@angular/material/select';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [DropdownComponent],
-  imports: [CommonModule, MatSelectModule],
+  imports: [CommonModule, MatSelectModule, ReactiveFormsModule],
   exports: [DropdownComponent],
 })
 export class DropdownModule {}
--- src/app/common/components/radio/radio.module.ts
+++ src/app/common/components/radio/radio.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RadioComponent } from './radio.component';
 import { MatRadioModule } from '@angular/material/radio';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [RadioComponent],
-  imports: [CommonModule, MatRadioModule],
+  imports: [CommonModule, MatRadioModule, ReactiveFormsModule],
   exports: [RadioComponent],
 })
 export class RadioModule {}
