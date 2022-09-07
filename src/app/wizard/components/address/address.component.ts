a7c1abdcbb45f37157f06dd161cce116fe2a0e25
Avtandil-1 <avtandil.merebashvili.2@gmail.com>
2022-09-07 09:32:28 +0400

    feat(address): crate form

--- src/app/wizard/components/address/address.component.ts
+++ src/app/wizard/components/address/address.component.ts
@@ -1,16 +1,84 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Options } from 'src/app/common/interfaces/options.interface';
+import { Controls } from 'src/app/common/models/form-components/controls.model';
+import { DropdownControl } from 'src/app/common/models/form-components/dropdown-control.model';
+import { RadioControl } from 'src/app/common/models/form-components/radio-control.model';
+import { TextControl } from 'src/app/common/models/form-components/text-control.model';
 
 @Component({
   selector: 'app-address',
   templateUrl: './address.component.html',
   styleUrls: ['./address.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AddressComponent implements OnInit {
+  countries: Options[] = [
+    {
+      view: 'საქართველო',
+      value: 1,
+    },
+    {
+      view: 'გერმანია',
+      value: 2,
+    },
+    {
+      view: 'ნორვეგია',
+      value: 3,
+    },
+    {
+      view: 'გრენლანდია',
+      value: 4,
+    },
+  ];
 
-  constructor() { }
+  stateOptions: Options[] = [
+    {
+      view: 'ახალი',
+      value: 1,
+    },
+    {
+      view: 'მეორადი',
+      value: 2,
+    },
+  ];
 
-  ngOnInit(): void {
-  }
+  doors: Options[] = [
+    {
+      view: '1',
+      value: 1,
+    },
+    {
+      view: '2',
+      value: 2,
+    },
+    {
+      view: '4',
+      value: 4,
+    },
+    {
+      view: '5',
+      value: 5,
+    },
+  ];
 
+  form = new FormGroup({
+    country: new FormControl(''),
+    city: new FormControl(''),
+    address1: new FormControl('', Validators.required),
+    address2: new FormControl(),
+  });
+
+  controls = new Controls([
+    [
+      new DropdownControl('country', 'ქვეყანა', this.countries),
+      new TextControl('city', 'ქალაქი'),
+    ],
+    [new TextControl('address1', 'მისამართი 1 *')],
+    [new TextControl('address2', 'მისამართი 2')],
+  ]);
+
+  constructor() {}
+
+  ngOnInit(): void {}
 }
